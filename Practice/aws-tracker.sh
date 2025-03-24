84034ec214c320143112ad61b03e90f82473b2c6
Irfan Ahmed <61529005+irffy@users.noreply.github.com>
2025-03-24 06:20:26 +0000

    ADDED aws script ec2 list

--- Practice/aws-tracker.sh
+++ Practice/aws-tracker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 aws s3 ls | awk '{print $3}'
-# aws ec2 describe-instances
+# aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text
